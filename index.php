b87ea0c60b708dfdf6c81c696880833de751b5e9
quinelle <mar13queen@gmail.com>
2021-02-05 19:20:14 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,54 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>stuff</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">    
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>CEO_NAME</p>
+                    <p>CEO_EMAIL</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/2.jpeg">
+                    <div class="container">
+                        <h2>CEO</h2>
+                        <p>CEO_NAME</p>
+                        <p>CEO_EMAIL</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                        <img src="images/3.jpeg">
+                        <div class="container">
+                            <h2>CEO</h2>
+                            <p>CEO_NAME</p>
+                            <p>CEO_EMAIL</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="column">
+                        <div class="card">
+                            <img src="images/4.jpeg">
+                            <div class="container">
+                                <h2>CEO</h2>
+                                <p>CEO_NAME</p>
+                                <p>CEO_EMAIL</p>
+                            </div>
+                        </div>
+                    </div>
+
+    </div>
+</body>
